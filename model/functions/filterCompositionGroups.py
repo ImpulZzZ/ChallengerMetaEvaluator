264b0909bff65b057f3ae3a582c11b043afd8aee
ImpulZzZ <lukas.drothler@web.de>
2021-04-24 14:42:22 +0200

    added filtering of compgroups by placement

--- model/functions/filterCompositionGroups.py
+++ model/functions/filterCompositionGroups.py
@@ -5,10 +5,9 @@ def filter_composition_groups(composition_groups, filters):
     # validate which filters are considered
     trait_filter_active     = len(filters["traits"])    > 0
     champion_filter_active  = len(filters["champions"]) > 0
-    placement_filter_active = filters["placements"]     != []
 
     # no filters set => do nothing
-    if not trait_filter_active and not placement_filter_active and not champion_filter_active:
+    if not trait_filter_active and not champion_filter_active:
         return composition_groups
 
     # filter for traits
@@ -37,5 +36,25 @@ def filter_composition_groups(composition_groups, filters):
             # append the composition group to result if filters are accepted
             if eligible:
                 result_composition_groups.append(current_comp_group)
+
+    return result_composition_groups
+
+
+def filter_composition_groups_by_placement(composition_groups, max_placement):
+    # list of resulting composition groups
+    result_composition_groups = []
+       
+    # loop over every composition group
+    for current_comp_group in composition_groups:
+
+        for composition in current_comp_group.compositions:
+
+            # append the composition group to current filtered composition group if placement is better or equal than filter
+            if composition.placement > max_placement:
+                current_comp_group.compositions.remove(composition)
+
+        # if the filtered composition group has more than 1 composition, append it to result
+        if len(current_comp_group.compositions) > 0:
+            result_composition_groups.append(current_comp_group)
 
     return result_composition_groups
