2f6c31d8104a404e7d99d9901792438c0eb4089d
ImpulZzZ <lukas.drothler@web.de>
2021-05-08 12:14:02 +0200

    added a dict field for easier item grouping

--- model/Composition.py
+++ model/Composition.py
@@ -12,8 +12,14 @@ class Composition:
         self.patch = patch
 
         self.champion_names = []
+        self.champion_item_dict = {}
+
         for champion in self.champions:
             self.champion_names.append(champion.name)
-        self.champion_names = sorted(self.champion_names)
+            self.champion_item_dict.update({champion.name : []})
+            for item in champion.items:
+                self.champion_item_dict[champion.name].append(int(item.id))
+            
+            self.champion_item_dict[champion.name] = sorted(self.champion_item_dict[champion.name])
 
-        
+        self.champion_names = sorted(self.champion_names)
