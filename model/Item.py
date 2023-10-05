e2aaf4db6a2ed5a8068c1e853b67911a7163be85
ImpulZzZ <lukas.drothler@web.de>
2023-10-05 16:41:28 +0200

    fixed attributes

--- model/Item.py
+++ model/Item.py
@@ -1,8 +1,8 @@
 class Item:
     def __init__(self, id, data):
         self.id = str(id)
-
-        self.icon = data.item_static_data[self.id]
+        self.name = data.item_static_data[self.id]["name"]
+        self.icon = data.item_static_data[self.id]["image"]
 
         #self.not_component = True if int(id) > 9 else False
         ## Not easily interpretable anymore
