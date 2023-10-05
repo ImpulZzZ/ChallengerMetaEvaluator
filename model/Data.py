b4d0f346c483db0a04fac15ac67451d6b3b747af
ImpulZzZ <lukas.drothler@web.de>
2023-10-05 19:04:15 +0200

    renamed methods

--- model/Data.py
+++ model/Data.py
@@ -1,17 +1,19 @@
-from model.jsonUtilities import extract_names_from_json, create_name_to_id_map, get_static_champion_data, get_static_item_data, create_name_to_icon_map, get_static_trait_data, extract_one_unit_traits_from_json
+from model.jsonUtilities import *
 
 class Data:
     def __init__(self):
-        self.current_patch             = "13.19"
-        self.data_dir                  = "Set9_5_data"
-        self.data_version              = "13.18.1"
-        self.api_key                   = open("apikey.txt", "r").read()
-        self.traits                    = extract_names_from_json(f"{self.data_dir}/traits.json")
-        self.champions                 = extract_names_from_json(f"{self.data_dir}/champions.json")
-        self.items                     = extract_names_from_json(f"{self.data_dir}/items.json")
-        self.item_name_to_id_map       = create_name_to_id_map(f"{self.data_dir}/items.json")
-        self.champion_static_data      = get_static_champion_data(f"{self.data_dir}/champions.json", f"{self.data_dir }/{self.data_version}/img/tft-champion")
-        self.trait_static_data         = get_static_trait_data(f"{self.data_dir}/traits.json", f"{self.data_dir }/{self.data_version}/img/tft-trait")
-        self.item_static_data          = get_static_item_data(f"{self.data_dir}/items.json", f"{self.data_dir }/{self.data_version}/img/tft-item")
-        self.champion_name_to_icon_map = create_name_to_icon_map(f"{self.data_dir}/champions.json", f"{self.data_dir }/{self.data_version}/img/tft-champion")
-        #self.one_unit_traits          = extract_one_unit_traits_from_json(f"{self.data_dir}/traits.json")
+        self.current_patch = "13.19"
+        self.data_dir      = "Set9_5_data"
+        self.data_version  = "13.18.1"
+        self.api_key       = open("apikey.txt", "r").read()
+
+        self.traits    = get_names_from_json(f"{self.data_dir}/traits.json")
+        self.champions = get_names_from_json(f"{self.data_dir}/champions.json")
+        self.items     = get_names_from_json(f"{self.data_dir}/items.json")
+
+        self.champion_static_data = get_static_champion_data(f"{self.data_dir}/champions.json", f"{self.data_dir }/{self.data_version}/img/tft-champion")
+        self.trait_static_data    = get_static_trait_data(f"{self.data_dir}/traits.json", f"{self.data_dir }/{self.data_version}/img/tft-trait")
+        self.item_static_data     = get_static_item_data(f"{self.data_dir}/items.json", f"{self.data_dir }/{self.data_version}/img/tft-item")
+        
+        self.champion_name_to_icon_map = get_name_to_icon_map(f"{self.data_dir}/champions.json", f"{self.data_dir }/{self.data_version}/img/tft-champion")
+        self.item_name_to_id_map       = get_name_to_id_map(f"{self.data_dir}/items.json")
--- model/jsonUtilities.py
+++ model/jsonUtilities.py
@@ -1,6 +1,6 @@
 import json
 
-def extract_names_from_json(json_file):
+def get_names_from_json(json_file):
     result = []
     with open(json_file) as jsonfile:
         data = json.load(jsonfile)
@@ -10,7 +10,7 @@ def extract_names_from_json(json_file):
 
     return sorted(result)
 
-def create_name_to_id_map(json_file):
+def get_name_to_id_map(json_file):
     result = {}
     with open(json_file) as jsonfile:
         data = json.load(jsonfile)
@@ -21,7 +21,7 @@ def create_name_to_id_map(json_file):
     return result
 
 
-def create_name_to_icon_map(json_file, path_to_img):
+def get_name_to_icon_map(json_file, path_to_img):
     result = {}
     with open(json_file) as jsonfile:
         data = json.load(jsonfile)
@@ -71,18 +71,5 @@ def get_static_item_data(json_file, path_to_img):
                                                         "image": f"{path_to_img}/{data['data'][item]['image']['full']}"
                                                       }
                           })
-
-    return result
-
-
-def extract_one_unit_traits_from_json(json_file):
-    result = []
-    with open(json_file) as jsonfile:
-        data = json.load(jsonfile)
-
-        for current in data["data"]:
-            for set in current["sets"]:
-                if set["min"] == 1:
-                    result.append(current["name"])
 
     return result
