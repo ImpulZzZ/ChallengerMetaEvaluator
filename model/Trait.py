5f58a634d44d016827f6686c0cace66c798bc3fb
ImpulZzZ <lukas.drothler@web.de>
2023-10-05 19:22:33 +0200

    changed single unit handling

--- model/Trait.py
+++ model/Trait.py
@@ -1,11 +1,9 @@
 class Trait:
-    def __init__(self, id, style, num_units, data):
+    def __init__(self, id, data, style=0):
         self.id        = str(id)   ## Name of trait that is given by api
         self.name      = data.trait_static_data[self.id]["name"]
         self.icon      = data.trait_static_data[self.id]["image"]
         self.style     = style     ## Current active level (0 = inactive, 1 = bronze, 2 = silver, 3 = gold, 4 = prismatic)
-        self.num_units = num_units ## Number of units that have this trait
 
-    def is_single_unit_trait(self):
-        if self.num_units == 1: return True
-        return False
+        if self.name in data.single_unit_traits: self.single_unit_trait = True
+        else: self.single_unit_trait = False
